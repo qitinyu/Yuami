fa6ea902f81fca01b1bf2733978616e28f03ee59
YuQi <484894496@qq.com>
2026-05-26 20:34:03 +0800

    更新 footer.ts

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -3,16 +3,16 @@ export const footerConfig = {
   // 版权信息
   copyright: {
     year: 2026,
-    owner: 'qitinyu',
+    owner: 'YuQi',
     ownerUrl: 'https://github.com/qitinyu',
-    siteUrl: 'https://yqamm.cc.cd',
+    siteUrl: 'https://8872388.xyz',
     email: '484894496@qq.com',
   },
   // 技术信息
   powered: {
     framework: 'Astro',
     theme: 'Yuami',
-    version: '1.0',
+    version: '1.0.6',
   },
   // 备案信息（可选，留空则不显示）
   icp: '',
